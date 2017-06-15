bcd442429f7bf7f30072f00d25556eeaf67ac0b1
wolfreak99 <jbminor1991@gmail.com>
2017-06-15 05:38:11 -0400

    Compiling fixes

--- audio-router-gui/app_list.cpp
+++ audio-router-gui/app_list.cpp
@@ -26,10 +26,10 @@ bool app_list::populate_list() {
 bool app_list::populate_list(const filters_t & filters) {
     this->apps.clear(); 
 
-    if ( ! this-> populate_list(true, filters))
+    if (!this->populate_list(true, filters))
         return false; 
 #ifdef _WIN64
-    return this -  > populate_list(false, filters); 
+    return this->populate_list(false, filters); 
 #else
     return true; 
 #endif
